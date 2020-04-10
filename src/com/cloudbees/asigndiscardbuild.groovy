ea34af1f82d17597d759d492c5a10c3ff0abc0e5
Ignacio Roncero Bazarra <ironcero@cloudbees.com>
2020-04-10 16:22:20 +0200

    1

--- src/com/cloudbees/asigndiscardbuild.groovy
+++ src/com/cloudbees/asigndiscardbuild.groovy
@@ -2,20 +2,24 @@ package com.cloudbees
 
 import jenkins.model.Jenkins
 
-def asignDiscardBuildToAllJobs(){
-    Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
-        if (job.isBuildable() && job.supportsLogRotator() && job.getProperty(jenkins.model.BuildDiscarderProperty) == null) {
-            println "Processing \"${job.fullDisplayName}\""
-                // adding a property implicitly saves so no explicit one
-                try {
-                    job.setBuildDiscarder(new hudson.tasks.LogRotator ( 3, 4, 5, 6))
-                    println "${job.fullName} is updated"
-                } catch (Exception e) {
-                    // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
-                    println "[WARNING] Failed to update ${job.fullName} of type ${job.class} : ${e}"
-                }
-        
+public class AsignDiscardBuild{
+
+    def asignDiscardBuildToAllJobs(){
+        Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
+            if (job.isBuildable() && job.supportsLogRotator() && job.getProperty(jenkins.model.BuildDiscarderProperty) == null) {
+                println "Processing \"${job.fullDisplayName}\""
+                    // adding a property implicitly saves so no explicit one
+                    try {
+                        job.setBuildDiscarder(new hudson.tasks.LogRotator ( 3, 4, 5, 6))
+                        println "${job.fullName} is updated"
+                    } catch (Exception e) {
+                        // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
+                        println "[WARNING] Failed to update ${job.fullName} of type ${job.class} : ${e}"
+                    }
+            
+            }
         }
+        return;
     }
-    return;
+
 }
