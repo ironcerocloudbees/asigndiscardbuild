2f79c3d6332dd3d5ab4926ecb5e82209df0ce0b2
Ignacio Roncero <61473809+ironcerocloudbees@users.noreply.github.com>
2020-04-15 15:42:53 +0200

    Update Asigndiscardbuild.groovy

--- vars/Asigndiscardbuild.groovy
+++ vars/Asigndiscardbuild.groovy
@@ -2,20 +2,26 @@ package com.cloudbees
 
 import jenkins.model.Jenkins
 
-def call(){
-    Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
-        if (job.isBuildable() && job.supportsLogRotator() && job.getProperty(jenkins.model.BuildDiscarderProperty) == null) {
-            this.println "Processing \"${job.fullDisplayName}\""
-                // adding a property implicitly saves so no explicit one
-                try {
-                    job.setBuildDiscarder(new hudson.tasks.LogRotator ( 3, 4, 5, 6))
-                    this.println "${job.fullName} is updated"
-                } catch (Exception e) {
-                    // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
-                    this.println "[WARNING] Failed to update ${job.fullName} of type ${job.class} : ${e}"
-                }
-        
+class AsignDiscargBuildUtil implements Serializable{
+    def steps
+    
+    AsignDiscargBuildUtil(steps) {this.steps = steps}
+    
+    def settingDiscardBuilds(){
+        Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
+            if (job.isBuildable() && job.supportsLogRotator() && job.getProperty(jenkins.model.BuildDiscarderProperty) == null) {
+                steps.println "Processing \"${job.fullDisplayName}\""
+                    // adding a property implicitly saves so no explicit one
+                    try {
+                        job.setBuildDiscarder(new hudson.tasks.LogRotator ( 3, 4, 5, 6))
+                        steps.println "${job.fullName} is updated"
+                    } catch (Exception e) {
+                        // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
+                        steps.println "[WARNING] Failed to update ${job.fullName} of type ${job.class} : ${e}"
+                    }
+
+            }
         }
+        return;
     }
-    return;
 }
