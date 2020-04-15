b9a35765283948ee5239cbcf90932e229cd60a08
Ignacio Roncero Bazarra <ironcero@cloudbees.com>
2020-04-15 16:52:06 +0200

    1

--- vars/Asigndiscardbuild.groovy
+++ vars/Asigndiscardbuild.groovy
@@ -3,17 +3,17 @@ package com.cloudbees
 import jenkins.model.Jenkins
 
 def call(){
-    sh 'echo "Starting"'
+    printf "Starting"
     Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
         if (job.isBuildable() && job.supportsLogRotator()) {
-            sh 'echo "Processing"'
+            printf "Processing"
                 // adding a property implicitly saves so no explicit one
                 try {
                     job.setBuildDiscarder(new hudson.tasks.LogRotator ( 10, 10, 10, 10))
-                    sh 'echo "is updated"'
+                    printf "is updated"
                 } catch (Exception e) {
                     // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
-                    sh 'echo "[WARNING] Failed to update"'
+                    printf "[WARNING] Failed to update"
                 }
 
         }
