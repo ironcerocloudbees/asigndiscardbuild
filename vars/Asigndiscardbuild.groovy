6a7506a1a6a130fd842cc0f435dd2a6ec7fc9267
Ignacio Roncero Bazarra <ironcero@cloudbees.com>
2020-04-15 16:48:19 +0200

    1

--- vars/Asigndiscardbuild.groovy
+++ vars/Asigndiscardbuild.groovy
@@ -8,14 +8,14 @@ def call(){
         if (job.isBuildable() && job.supportsLogRotator()) {
             def fullDisplayName = job.fullDisplayName.toString()
             def fullName = job.fullName.toString()
-            sh 'echo "Processing \"${fullDisplayName}\""'
+            sh 'echo "Processing"'
                 // adding a property implicitly saves so no explicit one
                 try {
                     job.setBuildDiscarder(new hudson.tasks.LogRotator ( 10, 10, 10, 10))
-                    sh 'echo "${fullName} is updated"'
+                    sh 'echo "is updated"'
                 } catch (Exception e) {
                     // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
-                    sh 'echo "[WARNING] Failed to update ${fullName} : ${e}"'
+                    sh 'echo "[WARNING] Failed to update"'
                 }
 
         }
