1ee56d6363bba2f17c4dbe0516e411fa539578eb
Ignacio Roncero Bazarra <ironcero@cloudbees.com>
2020-04-15 16:46:48 +0200

    1

--- vars/Asigndiscardbuild.groovy
+++ vars/Asigndiscardbuild.groovy
@@ -6,14 +6,16 @@ def call(){
     sh 'echo "Starting"'
     Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
         if (job.isBuildable() && job.supportsLogRotator()) {
-            sh 'echo "Processing \"${job.fullDisplayName}\""'
+            def fullDisplayName = job.fullDisplayName.toString()
+            def fullName = job.fullName.toString()
+            sh 'echo "Processing \"${fullDisplayName}\""'
                 // adding a property implicitly saves so no explicit one
                 try {
                     job.setBuildDiscarder(new hudson.tasks.LogRotator ( 10, 10, 10, 10))
-                    sh 'echo "${job.fullName} is updated"'
+                    sh 'echo "${fullName} is updated"'
                 } catch (Exception e) {
                     // Some implementation like for example the hudson.matrix.MatrixConfiguration supports a LogRotator but not setting it
-                    sh 'echo "[WARNING] Failed to update ${job.fullName} of type ${job.class} : ${e}"'
+                    sh 'echo "[WARNING] Failed to update ${fullName} : ${e}"'
                 }
 
         }
